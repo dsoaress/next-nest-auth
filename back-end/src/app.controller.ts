9c0fd3d795ed78461f68684301dbaeee1e66ef8c
Daniel Soares <daniel.soares@me.com>
2021-09-03 10:46:15 -0300

    fix(back): public root route

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common'
 
+import { Public } from '@/decorators/public-route.decorator'
+
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   getStatus() {
     return { status: '🚀 running' }
